635c3f55191e286f68e677b6573416bc32ea6135
Seth Mwebi <sethmwebi27@gmail.com>
2024-09-04 14:41:38 +0300

    adds code for python instances

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-class Dog:
-    # Class body goes here
 
-    #Instance method definition
-    pass
+class Dog:
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+
+fido = Dog()
+fido.bark()
+
+snoopy = Dog()
+snoopy.bark()
